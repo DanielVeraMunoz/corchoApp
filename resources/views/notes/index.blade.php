bb28070385748ff9e2feca12d2e0fe8078146b40
Daniel Vera <dani.vmg91@gmail.com>
2026-03-06 21:45:46 +0100

    Edit notes view

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -4,10 +4,10 @@
         <div class="bg-green-100 overflow-hidden shadow-sm sm:rounded-lg mb-4 p-4">
             <a href="{{ route('notes.create') }}">Crear nueva nota</a>
         </div>
-        <div class="flex justify-center">
 
-        <h1>¡Hola vecina!</h1>
-        <p>Echa un vistazo a lo que está pasando por el bloque</p>
+        <div class="flex justify-center flex-col items-center mb-10">
+            <h1 class="text-orange-500 italic text-5xl mb-5">¡Hola vecina!</h1>
+            <p>Echa un vistazo a lo que está pasando por el bloque.</p>
         </div>
         
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-center">
@@ -19,10 +19,10 @@
                 @foreach($notes as $note)
 
                 <div class="break-inside-avoid mb-6 mx-auto bg-white border border-gray-950 overflow-hidden shadow-sm m-0 p-4 w-80 md:w-60">
-
-                    <h2>{{ $note->title }}</h2>
+                    <p class="text-lg font-semibold text-gray-700 mb-2">Categoría {{ $note->category }}</p>
+                    <h2 class="text-xl font-bold text-gray-800 mb-5">{{ $note->title }}</h2>
                     <p>{{ $note->description }}</p>
-
+                    <div class="border-b border-gray-800 my-5"></div>
                     <form action="{{ route('notes.destroy', $note->id) }}" method="POST">
                     @csrf
                     @method('DELETE')
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                jaro: ['Jaro', ...defaultTheme.fontFamily.sans],
             },
         },
     },
