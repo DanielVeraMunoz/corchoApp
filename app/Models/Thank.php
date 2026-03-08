43a012bc98472ca3758a84db0391dee2bf74f3f3
Daniel Vera <dani.vmg91@gmail.com>
2026-03-08 12:06:35 +0100

    Implement MVC for Thanks

--- app/Models/Thank.php
+++ app/Models/Thank.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thank extends Model
 {
-    //
+    protected $fillable = ['user_id', 'note_id'];
 }
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::middleware('auth')->group(function () {
     Route::resource('communities', \App\Http\Controllers\CommunityController::class);
     Route::resource('categories', \App\Http\Controllers\CategoryController::class);
     Route::resource('comments', \App\Http\Controllers\CommentController::class);
+    Route::resource('thanks', \App\Http\Controllers\ThankController::class);
     
 });
 
