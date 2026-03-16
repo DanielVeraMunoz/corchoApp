51df4d3f7da723c72080b891e32aa2712c205b39
Daniel Vera <dani.vmg91@gmail.com>
2026-03-16 12:51:34 +0100

    Fix views auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('Nombre')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
@@ -18,7 +18,7 @@
 
         <!-- Community -->
         <div class="mt-4">
-            <x-input-label for="community_id" :value="__('Community')" />
+            <x-input-label for="community_id" :value="__('Comunidad')" />
             <select id="community_id" name="community_id" class="block mt-1 w-full border-gray-300 rounded-md shadow-sm" required>
                 <option value="">Select a community</option>
                 <option value="1">Comunidad 1</option>
@@ -28,19 +28,19 @@
 
         <!-- Floor -->
         <div class="mt-4">
-            <x-input-label for="floor" :value="__('Floor')" />
+            <x-input-label for="floor" :value="__('Piso')" />
             <x-text-input id="floor" class="block mt-1 w-full" type="number" name="floor" required />
         </div>
         
         <!-- Door -->
         <div class="mt-4">
-            <x-input-label for="door" :value="__('Door')" />
+            <x-input-label for="door" :value="__('Puerta')" />
             <x-text-input id="door" class="block mt-1 w-full" type="number" name="door" required />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Contraseña')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -52,7 +52,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Confirmar contraseña')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -63,11 +63,11 @@
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+                {{ __('¿Ya tienes una cuenta?') }}
             </a>
 
             <x-primary-button class="ms-4">
-                {{ __('Register') }}
+                {{ __('Registrarse') }}
             </x-primary-button>
         </div>
     </form>
